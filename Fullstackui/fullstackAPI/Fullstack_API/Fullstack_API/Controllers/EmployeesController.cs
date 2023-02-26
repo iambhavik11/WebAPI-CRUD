4e0108bc9ed30e52cbef01302895419431ce0fba
iambhavik11 <66296381+iambhavik11@users.noreply.github.com>
2023-02-26 12:07:10 +0530

    First Commit

---
+++ Fullstackui/fullstackAPI/Fullstack_API/Fullstack_API/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using Fullstack_API.Data;
+using Fullstack_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fullstack_API.Controllers
+{
+    [ApiController] // Tells it is a API Controller
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullstackDbContext _fullstackDbContext;
+        public EmployeesController(FullstackDbContext fullstackDbContext)
+        {
+            _fullstackDbContext = fullstackDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _fullstackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid(); // generates random id
+            await _fullstackDbContext.Employees.AddAsync(employeeRequest); // add the employee
+            await _fullstackDbContext.SaveChangesAsync(); // save the changes
+            return Ok(employeeRequest);
+        }
+    }
+}
---
+++ Fullstackui/fullstackAPI/Fullstack_API/Fullstack_API/Data/FullstackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Fullstack_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fullstack_API.Data
+{
+    // Inject it in Program.cs / Startup.cs before build with Connection string
+    public class FullstackDbContext : DbContext
+    {
+        public FullstackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
